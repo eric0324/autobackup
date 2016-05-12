3f5ce7f31a6e86a2123195d06f532da0be89ff57
lck <s1023352@mail.yzu.edu.tw>
2016-05-13 00:40:43 +0800

    arg -l

--- env_bk.sh
+++ env_bk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TIME=`date +%Y%m%d`
 DESDIR=/home/`whoami`/env_backup
+#scan name
 Package=(vim ruby python atom sublime xcode nodejs)
 if [[ ! -e $DESDIR ]]; then
 	mkdir $DESDIR
@@ -9,30 +9,29 @@ elif [[ -f $DESDIR ]]; then
 	exit 1
 fi
 
-#vim
-#rsync -ut ~/.zshrc $DESDIR
 if [[ $1 == '-a' ]] || [[ $1 == '--auto' ]]; then
 	#scan & backup
 	for i in "${Package[@]}"; do
 		if [[ $( dpkg -s $i 2> /dev/null | grep Status ) == 'Status: install ok installed' ]]; then
 			#crontab 
-			echo "$i"
-			BK_TIME="$DESDIR/$TIME"
+			#echo "$i"
+			BK_TIME="$DESDIR/`date +%Y%m%d`"
 			mkdir $BK_TIME
 			if [[ $i == 'vim' ]]; then
 				mkdir $BK_TIME/vim
-
+				cp /home/`whoami`/.vimrc $BK_TIME/vim/.vimrc
 			fi	
 		fi
 	done
 #elif [[ $1 == '-u' ]] || [[ $1 == '--add' ]]; then
 	#statements	
-#elif [[ $1 == 'l' ]] || [[ $1 == '--list' ]]; then
+elif [[ $1 == '-l' ]] || [[ $1 == '--list' ]]; then
+	ls -a $DESDIR/
+#elif [[ $1 == '-r' ]] || [[ $1 == '--restore' ]]; then
 	#statements
-#elif [[ $1 == 'r' ]] || [[ $1 == '--restore' ]]; then
-	#statements
-#elif [[ $1 == 'd' ]] || [[ $1 == '--delete' ]]; then
-	#statements
+elif [[ $1 == '-d' ]] || [[ $1 == '--delete' ]]; then
+	echo `ls $DESDIR | grep $2`
+	#rm -rf $2
 else
 	echo "Wrong argument: $1" >&2
 	exit 2
