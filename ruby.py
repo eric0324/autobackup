a35c55482d4469768dd3761ac9f2f0f52577222d
Eric Wu <smart032410@gmail.com>
2016-06-02 11:44:29 +0800

    runy install

--- ruby.py
+++ ruby.py
@@ -1,16 +1,37 @@
 import os
+import sys
 import subprocess
 import string
 
-cmd = "gem list --local"
-result = subprocess.check_output(cmd, shell=True)
-result = result.replace(" (", "%%")
-result = result.replace(")"," ")
-result = result.replace("\n", "##")
+def backup():
+    try:
+        cmd = "gem list --local"
+        result = subprocess.check_output(cmd, shell=True)
+        result = result.replace(" (", "%%")
+        result = result.replace(")"," ")
+        result = result.replace("\n", "##")
 
-f = open('ruby_backup.txt', 'wb')
-f.write("##")
-f.write(result)
-f.close()
+        f = open('ruby_backup.txt', 'wb')
+        f.write("##")
+        f.write(result)
+        f.close()
 
-print "Ruby backup success!"
+        print "Ruby backup success!"
+        return
+    except:
+        print "Error: Ruby backup fail"
+
+def reinstill():
+    argv = sys.argv
+
+    cmd = "gem install " + argv[2]
+    result = subprocess.check_output(cmd, shell=True)
+
+
+#Main function 
+argv = sys.argv
+
+if argv[1] == 'backup':
+    backup()
+elif argv[1] == 'reinstall':
+    reinstall()
