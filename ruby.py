0acb32c7949db05ef263904e751ab725edb423c4
Eric Wu <smart032410@gmail.com>
2016-06-02 10:55:54 +0800

    ruby backup

--- ruby.py
+++ ruby.py
@@ -1,4 +1,14 @@
 import os
 import subprocess
-cmd="gem list --local"
+import string
+
+cmd = "gem list --local"
 result = subprocess.check_output(cmd, shell=True)
+result = result.replace(" (", "%%")
+result = result.replace(")"," ")
+result = result.replace("\n", "##")
+
+f = open('ruby_backup.txt', 'wb')
+print result
+f.write("##")
+f.write(result)
