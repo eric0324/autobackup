e4603ef89a9256edc0f66a2244c6ee51418ced15
lck <s1023352@mail.yzu.edu.tw>
2016-06-04 18:27:19 +0800

    restore features

--- env_bk.sh
+++ env_bk.sh
@@ -5,7 +5,6 @@ Package=(vim ruby python atom sublime xcode nodejs git)
 nInstall=()
 cronCMD='@daily '
 if [[ ! -e $DESDIR ]]; then
-	#mkdir $DESDIR
 	git init $DESDIR
 elif [[ -f $DESDIR ]]; then
 	echo "can't create dir: $DESDIR" >&2
@@ -26,14 +25,14 @@ if [[ $1 == '-a' ]] || [[ $1 == '--auto' ]]; then
 				if [[ ! -e $DESDIR/gitconfig ]]; then
 					cp -p ~/.gitconfig $DESDIR/gitconfig
 				fi
-				cronCMD+="bash `pwd`/git.sh -b"
+				cronCMD+="bash `pwd`/git.sh -b;"
 			elif [[ $i == 'ruby' ]]; then
 				cronCMD+="python `pwd`/ruby.py -b||"
 			elif [[ $i == 'python' ]]; then
 				cronCMD+="python `pwd`/python.py -b||"
 				pip list | tr -d "()" > $DESDIR/python_env
 			elif [[ $i == 'atom' ]]; then
-				cronCMD+="python `pwd`/atom.py -b||;"
+				cronCMD+="python `pwd`/atom.py -b||"
 			elif [[ $i == 'sublime' ]]; then
 				cronCMD+="python `pwd`/sublime.py -b||"
 			elif [[ $i == 'xcode' ]]; then
@@ -55,23 +54,29 @@ elif [[ $1 == '-l' ]] || [[ $1 == '--list' ]]; then
 	ls -a $DESDIR/
 elif [[ $1 == '-r' ]] || [[ $1 == '--restore' ]]; then
 	if [[ $2 == '' ]]; then
+		cd $DESDIR
 		echo "which environment you want to restore"
-		arr=($(ls ~/env_backup))
+		arr=($(ls $DESDIR))
 		for (( i = 0; i < ${#arr[@]}; i++ )); do
 			echo "$((i+1)). ${arr[i]}"
 		done
 		printf "input a number: "
 		read num
-		restore_env=${arr[$((num - 1))]}
-		echo "which one you want to restore"
-		arr=($(ls ~/env_backup/$restore_env))
-		for (( i = 0; i < ${#arr[@]}; i++ )); do
-			echo "$((i+1)). ${arr[i]}"
-		done
-		printf "input a number: "
+		$2=${arr[$((num - 1))]}
+		echo "which day you want to restore"
+		git log --pretty=format:"%s"
+		printf "input date: "
+		read num
+		$3=`git log --pretty=format:"%s %H" | awk /$num/'{print $2}'`
+		cd -
+	fi
+	if [[ $2 == 'vim' ]]; then
+		bash ./vim.sh -r $3
 	elif [[ $2 == 'python' ]]; then
-		ls $DESDIR/python_env
-		#arr=($(awk '{print $1}' filename))
+		arr=($(awk '{print $1}' $DESDIR/python))
+		for name in "${arr[@]}"; do
+			python ./python.py -r $name
+		done
 	fi
 elif [[ $1 == '-d' ]] || [[ $1 == '--delete' ]]; then
 	echo `ls $DESDIR | grep $2`
