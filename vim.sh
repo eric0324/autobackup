095a6f980fb783f6dcbf6a9529f4b58792665958
lck <s1023352@mail.yzu.edu.tw>
2016-06-04 18:24:50 +0800

    delete git commit

--- vim.sh
+++ vim.sh
@@ -3,27 +3,22 @@ DESDIR=/home/`whoami`/.`whoami`_env
 
 backup()
 {
-	cp -p ~/.vimrc $DESDIR/vimrc
-	cd $DESDIR
-	git add vimrc
-	git commit -m"vim`date +%Y%m%d`"
-	#git push
+	cp -p ~/.vimrc $DESDIR/vim
 }
 
 reinstall()
 {
 	cd $DESDIR
-	git checkout $1 vimrc
-	cp -p $DESDIR/vimrc ~/.vimrc
+	git checkout $1 vim
+	cp -p $DESDIR/vim ~/.vimrc
 	vim +PluginInstall +qall
-	git checkout HEAD vimrc
+	git checkout HEAD vim
 }
 
 if [[ $1 == '-b' ]]; then
 	backup
 elif [[ $1 == '-r' ]]; then
-	cp -p $DESDIR/vimrc ~/.vimrc
-	vim +PluginInstall +qall
+	reinstall $2
 elif [[ $1 == '-c' ]]; then
 	if [[ `find ~/ -name ".vimrc" -mtime -1` == '.vimrc' ]]; then
 		backup
