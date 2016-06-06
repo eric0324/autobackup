cf8219101593ba33694e4edb6774c3a25a3723a5
Eric Wu <smart032410@gmail.com>
2016-06-06 09:55:01 +0800

    atom

--- atom.py
+++ atom.py
@@ -5,32 +5,19 @@ import subprocess
 import string
 import re
 
-def backup():
+filepath = "atom_backup.txt"
+
+try:
     cmd = "apm list --bare"
     result = subprocess.check_output(cmd, shell=True)
     result = result.replace("@", "%%")
     result = result.replace("\n", "##")
     result = result.replace("####", "")
-    f = open('atom_backup.txt', 'wb')
+    f = open(filepath, 'wb')
     f.write("##")
     f.write(result)
     f.close()
 
     print "Atom env backup success!"
-
-
-def reinstall():
-    argv = sys.argv
-
-    cmd = "apm install " + argv[2]
-    result = subprocess.check_output(cmd, shell=True)
-    print "Reduce " + argv[2] + " success!"
-
-
-#Main function
-argv = sys.argv
-
-if argv[1] == '-b':
-    backup()
-elif argv[1] == '-r':
-    reinstall()
+except:
+    print "Error: Atom backup fail"
