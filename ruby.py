6648b47156673174ab79dc2d6102fb910e162253
Eric Wu <smart032410@gmail.com>
2016-06-06 09:53:05 +0800

    rb

--- ruby.py
+++ ruby.py
@@ -3,36 +3,20 @@ import sys
 import subprocess
 import string
 
-def backup():
-    try:
-        cmd = "gem list --local"
-        result = subprocess.check_output(cmd, shell=True)
-        result = result.replace(" (", "%%")
-        result = result.replace(")"," ")
-        result = result.replace("\n", "##")
+filepath = "ruby_backup.txt"
 
-        f = open('ruby_backup.txt', 'wb')
-        f.write("##")
-        f.write(result)
-        f.close()
-
-        print "Ruby env backup success!"
-        return
-    except:
-        print "Error: Ruby backup fail"
-
-def reinstall():
-    argv = sys.argv
-
-    cmd = "gem install " + argv[2]
+try:
+    cmd = "gem list --local"
     result = subprocess.check_output(cmd, shell=True)
-    print "Reduce " + argv[2] + " success!"
+    result = result.replace(" (", "%%")
+    result = result.replace(")"," ")
+    result = result.replace("\n", "##")
 
+    f = open(filepath, 'wb')
+    f.write("##")
+    f.write(result)
+    f.close()
 
-#Main function
-argv = sys.argv
-
-if argv[1] == '-b':
-    backup()
-elif argv[1] == '-r':
-    reinstall()
+    print "Ruby env backup success!"
+except:
+    print "Error: Ruby backup fail"
