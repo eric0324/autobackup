90601154951d6cc0f5e81858f184edbdf7a20a90
lck <s1023352@mail.yzu.edu.tw>
2016-06-08 17:32:48 +0800

    set timer

--- env_bk.sh
+++ env_bk.sh
@@ -17,6 +17,8 @@ elif [[ -f $DESDIR ]]; then
 	echo "can't create dir: $DESDIR" >&2
 	exit 1
 fi
+job=`crontab -l 2> /dev/null`
+crontab -l 2> /dev/null | grep -v `pwd` | crontab -
 case $1 in
 	-s | --scan )
 	#scan & backup
@@ -30,37 +32,54 @@ case $1 in
 			nInstall+=($i)
 		fi
 	done
+	cronCMD+=" bash `pwd`/upload.sh;"
 	#echo "$cronCMD"
 	for n in "${nInstall[@]}" ; do
 		echo "$n is not installed"
 	done
-	#crontab -l | { cat; echo "@daily bash upload.sh";} | crontab -
+	#job+=`crontab -l | { cat; echo "$cronCMD";}`
 		;;
 	-r | --recover )
-	if [[ $2 == '' ]]; then
-		cd $DESDIR
-		echo "which environment you want to restore"
-		arr=(all)
-		arr+=($(ls $DESDIR))
-		for (( i = 0; i < ${#arr[@]}; i++ )); do
-			echo "$((i+1)). ${arr[i]}"
+	cd $DESDIR
+	arr=(all)
+	case $# in
+		2 )
+			echo "which environment you want to restore"
+			arr+=($(ls $DESDIR))
+			for (( i = 0; i < ${#arr[@]}; i++ )); do
+				echo "$((i+1)). ${arr[i]}"
+			done
+			printf "input a number: "
+			read num
+			set -- "{@:1}" "${arr[$((num - 1))]}" 
+			;&
+		3 )
+			echo "which day you want to restore"
+			git log --pretty=format:"%s"
+			printf "input date: "
+			read mdate 
+			set -- "{@:2}" "`git log --pretty=format:"%s %H" | awk /$mdate/'{print $2}'`"
+			;;
+	esac
+	git checkout $3 .
+	cd -
+	if [[ $2 == 'all' ]]; then
+		for (( i = 1; i < ${#arr[@]}; i++ )); do
+			python ./backup.py -${arr[i]}
 		done
-		printf "input a number: "
-		read num
-		echo "which day you want to restore"
-		git log --pretty=format:"%s"
-		printf "input date: "
-		read mdate 
-		set -- "{@:1}" "${arr[$((num - 1))]}" "`git log --pretty=format:"%s %H" | awk /$mdate/'{print $2}'`"
-		cd -
+	else
+		python ./recover.py -$2
 	fi
-	python ./recover.py -$2 $3
+	cd $DESDIR
+	git checkout HEAD .
+	cd -
 		;;
 	-[Sm] | --set )
 		if [[ $2 == 'dir' ]]; then
 			awk -v dir=$3 '{ if(/DESDIR/) print "DESDIR="dir; else print $0 }' ./.config > /tmp/config.tmp 
 			mv /tmp/config.tmp ./.config
-			python ./recovery.py -all HEAD
+		elif [[ $2 == 'timer' ]]; then
+			echo "todo: set crontab interval"
 		fi
 		;;
 	-l | --list)
@@ -71,5 +90,5 @@ case $1 in
 	exit 2
 		;;
 esac
-
+printf $job | crontab -
 exit 0
