c88c9a4fbb2e912e4b792d7c7add35a3dcc69508
lck <s1023352@mail.yzu.edu.tw>
2016-06-13 21:41:24 +0800

    change name dir name & title

--- env_init.sh
+++ env_init.sh
@@ -1,50 +1,27 @@
 #!/bin/sh
 echo "input your github acc" 
 read user
-dirname="mybackup"  #name of github repo
-
-
+dirname="env_backup"  #name of github repo
 
 if ! [ -f "/home/$USER/.ssh/id_rsa" ];then
-
-ssh-keygen -t rsa
-
+	ssh-keygen -t rsa
 fi
 
+key=`cat ~/.ssh/id_rsa.pub`
+echo "create repo..."
 
-
-
-cd ~/.ssh
-while read rsa_key
-do
-key=$rsa_key
-done < 'id_rsa.pub'
-
-
-
-curl -u $user https://api.github.com/user/keys -d "{\"title\": \"bka\",\"key\": \"$key\" }"
-
+curl -u $user https://api.github.com/user/keys -d "{\"title\": \"env ssh key\",\"key\": \"$key\" }"
 curl -u $user https://api.github.com/user/repos -d "{\"name\":\"$dirname\"}"
-
-
 
 ssh-add id_rsa
 
 ssh -T git@github.com
 
+git clone git@github.com:$user/$dirname.git $1
 
-
-
-git clone git@github.com:$user/$dirname.git ~/.env
-
-
-cd ~/.env
+cd $1
 
 git config user.name $user
-
-
 git config user.email $user
 
-
 echo "done"
-
