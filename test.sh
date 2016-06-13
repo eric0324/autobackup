ef927422b810cb68062b3521a8a1e80539af0fa8
Michaelma <shao23002@gmail.com>
2016-06-13 14:40:41 +0800

    load id_key

--- test.sh
+++ test.sh
@@ -1,27 +1,55 @@
 #!/bin/sh
-echo "plz input ur github acc" 
+echo "input your github acc" 
 read user
+dirname="mybackup"  #name of github repo
 
 
-ssh-keygen -t rsa 
-#-C $user
+
+if ! [ -f "/home/$USER/.ssh/id_rsa" ];then
+
+ssh-keygen -t rsa
+
+fi
+
+
+
 
 cd ~/.ssh
 while read rsa_key
 do
 key=$rsa_key
 done < 'id_rsa.pub'
-#read key
 
-echo $key
+
 
 curl -u $user https://api.github.com/user/keys -d "{\"title\": \"bka\",\"key\": \"$key\" }"
 
-curl -u $user https://api.github.com/user/repos -d "{\"name\":\"mybackup\"}"
+curl -u $user https://api.github.com/user/repos -d "{\"name\":\"$dirname\"}"
+
+git init
+
+ssh-add id_rsa
 
 ssh -T git@github.com
 
-#git remote add origin git@github.com:$user/mybackup.git
+cd Desktop/backup #cd to working file
+
+git remote add origin git@github.com:$user/$dirname.git
+
+
+
+git config user.mail "apple"
+git config user.name "apple"
+
+mv ~/.git ./.git
+
+
+
+#git commit -m "1"
+
+
+echo "done"
+
 
 
 
